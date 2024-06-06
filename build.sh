ec61c563e766632c049d1856732ff9ec9b1a3315
Oliver Tzeng <olivertzeng@proton.me>
2024-06-06 09:13:48 +0800

    fix(gitignore): aux tex log

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for f in ./**/*.tex; do
+	filename=$(basename "$f")
+	echo "Now compiling $filename..."
+	echo
+	read -rp "Press Enter to continue..." _
+	cd "$(dirname "$f")" || return
+	pwd
+	if [ -f "$filename" ]; then
+		xelatex "$filename" -synctex=1 -interaction=nonstopmode --shell-escape --enable-8bit-chars -recorder
+	else
+		echo "Error: File not found: $filename"
+	fi
+	read -rp "Press Enter to continue..." _
+done
