b4709ba41b73f1ca32b305a106382eee022756ad
Oliver Tzeng <olivertzeng@proton.me>
2024-07-03 19:10:57 +0800

    feat(ZJ): f579

--- c/blocks.c
+++ c/blocks.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	int a, b, n;
+	int a, b, n, num, result = 0;
 	scanf("%d %d", &a, &b);
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
+		int A = 0, B = 0;
+		while (scanf("%d", &num) == 1 && num != 0) {
+			if (num == a) {
+				A = 1;
+			} else if (num == b) {
+				B = 1;
+			} else if (num == -a) {
+				A = 0;
+			} else if (num == -b) {
+				B = 0;
+			}
+		}
+		if (A && B) {
+			result++;
+		}
 	}
+	printf("result = %d\n", result);
 	return 0;
 }
