afd29a29d0b1a143fd61770d19cb31b52667b032
Oliver Tzeng <olivertzeng@proton.me>
2024-09-02 21:39:46 +0800

    feat(apcs): playlist

---
+++ cpp/apcs/I10.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+	std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
+	int n, record = 0, tmp = 0;
+	std::cin >> n;
+	std::vector<long> visited(n);
+	long k[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> k[i];
+	}
+	visited.clear();
+
+	for (auto &song : k) {
+		if (std::find(visited.begin(), visited.end(), song) == visited.end()) {
+			visited.push_back(song);
+			tmp++;
+		} else {
+			visited.clear();
+			record = std::max(record, tmp);
+			tmp = 1;
+		}
+	}
+	record = std::max(record, tmp);
+	std::cout << record << '\n';
+
+	return 0;
+}
