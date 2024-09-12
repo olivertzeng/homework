10d473835e377b0ab0055c925187a02ef2d6c928
Oliver Tzeng <olivertzeng@proton.me>
2024-09-12 01:02:39 +0000

    Update f202.cpp

--- cpp/homework/f202.cpp
+++ cpp/homework/f202.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <vector>
 
 int main(int argc, char *argv[]) {
 	std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
 	long long a, b, result = 0;
 	std::cin >> a >> b;
-	std::vector<long long> formula(b);
 	for (long long i = a; i < b + 1; i++) {
 		result += i;
 		if (i == b) {
