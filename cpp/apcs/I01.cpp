fac2742c943eed91976c0b343c12f3303e81f5c4
Oliver Tzeng <olivertzeng@proton.me>
2024-09-16 15:03:11 +0800

    feat(apcs): new stuff

--- cpp/apcs/I01.cpp
+++ cpp/apcs/I01.cpp
@@ -1,25 +1,10 @@
 #include <iostream>
 
-long long m, n, ans = 0;
-
-void solve(int i = 1, int s = 0) {
-	if (s + n - i > m)
-		return;
-	if (i == n) {
-		ans += s == m;
-		return;
-	}
-	for (long long x = 1; x <= i; x++) {
-		solve(i + 1, s + x);
-	}
-}
-
 int main(int argc, char *argv[]) {
 	std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
-	std::cin >> n;
-	long long p[n];
-	for (auto &points : p) {
-		std::cin >> points;
+	char usr[8][8];
+	for (int i = 0; i < 8; i++) {
+		std::cin >> usr[i];
 	}
 
 	return 0;
--- cpp/apcs/I02.cpp
+++ cpp/apcs/I02.cpp
@@ -1,33 +1,32 @@
 #include <algorithm>
 #include <iostream>
-#include <utility>
-#include <vector>
 
 int main(int argc, char *argv[]) {
 	std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
-	long long n, k, num;
+	long long k, num, usr[(int)2e5];
+	int n, ans = 0;
 	std::cin >> n >> k;
-	long long left = 0, right = n - 1;
-	std::vector<std::pair<long long, long long>> usr(n);
+	int left = 0, right = n - 1;
 	for (long long i = 0; i < n; i++) {
 		std::cin >> num;
-		usr[i] = {num, i + 1};
+		usr[i] = num;
 	}
 
-	std::sort(usr.begin(), usr.end());
+	std::sort(usr, usr + n);
 
 	while (left < right) {
-		long long sum = usr[left].first + usr[right].first;
-		if (sum == k) {
-			std::cout << usr[right].second << ' ' << usr[left].second << '\n';
-			return 0;
-		} else if (sum > k)
-			right--;
-		else
+		long long sum = usr[left] + usr[right];
+		if (sum <= k)
 			left++;
+		right--;
+		ans++;
 	}
 
-	std::cout << "IMPOSSIBLE";
+	if (left == right) {
+		ans++;
+	}
+
+	std::cout << ans << '\n';
 
 	return 0;
 }
