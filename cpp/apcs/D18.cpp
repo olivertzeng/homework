cbec79ceee082e493fb7097f3a4527e68285601c
Oliver Tzeng <olivertzeng@proton.me>
2024-09-26 20:55:56 +0800

    feat(mobility.csv): direct link test

--- cpp/apcs/D18.cpp
+++ cpp/apcs/D18.cpp
@@ -1,11 +1,17 @@
 #include <algorithm>
 #include <iostream>
+#include <utility>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
-	long long a, b;
+	long long a, b, sum;
 	std::cin >> a >> b;
-	std::max(a, b);
+	if (b < a)
+		std::swap(a, b);
+	a += a % 2;
+	b -= b % 2;
+	sum = (a + b) / 2 * ((b - a) / 2 + 1);
+	std::cout << sum << '\n';
 
 	return 0;
 }
---
+++ english/china/notes.md
@@ -0,0 +1,12 @@
+# The social problems between different classes in China
+
+---
+
+## 1
+* The boundaries between classes in China has incresingly solidify lately
+* In other words, intergenerational mobility has become less and less
+
+---
+
+## 2
+*
