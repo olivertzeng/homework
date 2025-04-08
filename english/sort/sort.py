415c000456e99fd8370b7960fba89978ba9c3e3e
Oliver Tzeng <olivertzeng@proton.me>
2025-04-08 17:05:19 +0800

    feat(whitelist mode): added whitelist mode

--- english/sort/sort.py
+++ english/sort/sort.py
@@ -3,105 +3,128 @@ import pathlib
 import re
 
 # Define constants
-BLOCKED_WORDS = [
+BLOCKED = [
+    "N",
+    "Ving",
     "a",
     "about",
     "an",
+    "and",
     "at",
     "be",
+    "by",
     "for",
     "in",
+    "it",
+    "itself",
     "of",
     "on",
     "one",
     "one's",
     "oneself",
+    "out",
+    "sb",
+    "sth",
     "the",
     "to",
+    "up",
+    "with",
+]
+UNBLOCKED = [
+    "by oneself",
+    "in itself",
+    "one another",
+    "one by one",
+    "one on one",
+    "up to N",
+    "up to",
 ]
 BLOCK_LABEL = "峀"
 NEWLINE_LABEL = "甭"
 PARENTHESIS_LABEL = "刂"
 
 
-def is_main_line(line):
+def isMain(l):
     """Check if a line is a main line (starts with a number + ',')."""
-    return re.match(r"^\d+,", line)
+    return re.match(r"^\d+,", l)
 
 
-def group_lines(lines):
+def blocked(l, n):
+    noUnblock = l.split()[n] in BLOCKED or re.search(r"\([^)]*\)", l.split()[n])
+    return not (l[: l.index(",")] in UNBLOCKED) and noUnblock
+
+
+def group(lines):
     result = []
-    main_line = None
-    for line in lines:
-        if is_main_line(line):
-            if main_line:
-                result.append(main_line)
-            main_line = line
+    mainLine = None
+    for l in lines:
+        if isMain(l):
+            if mainLine:
+                result.append(mainLine)
+            mainLine = l
         else:
-            if main_line:
-                main_line += NEWLINE_LABEL + line
-    if main_line:
-        result.append(main_line)
-    for i, line in enumerate(result):
-        result[i] = line[line.index(",") + 1 :]
+            if mainLine:
+                mainLine += NEWLINE_LABEL + l
+    if mainLine:
+        result.append(mainLine)
+    for i, l in enumerate(result):
+        result[i] = l[l.index(",") + 1 :]
     return result
 
 
-def block_lines(lines):
+def block(lines):
     """Add block labels until the nth word isn't blocked."""
     n = 0
-    for i, line in enumerate(lines):
+    for i, l in enumerate(lines):
         """Group parenthesis into a single word(setup)"""
-        if "(" in line and ")" in line:
+        if "(" in l and ")" in l:
             # find the substring within the parenthesis
-            start_idx = line.find("(")
-            end_idx = line.find(")")
+            start_idx = l.find("(")
+            end_idx = l.find(")")
 
             # replace extra spaces within the parenthesis
-            substring = re.sub(r"\s+", PARENTHESIS_LABEL, line[start_idx + 1 : end_idx])
+            substring = re.sub(r"\s+", PARENTHESIS_LABEL, l[start_idx + 1 : end_idx])
 
             # replace the original substring with the modified one
-            lines[i] = line[: start_idx + 1] + substring + line[end_idx:]
+            lines[i] = l[: start_idx + 1] + substring + l[end_idx:]
     while True:
         """the main proccess"""
         allBlocked = True
-        for i, line in enumerate(lines):
-            words = line.split()
-            if words[n] in BLOCKED_WORDS or re.search(r"\([^)]*\)", words[n]):
+        for i, l in enumerate(lines):
+            if blocked(l, n):
                 allBlocked = False
                 break
         if allBlocked:
             break
-        for i, line in enumerate(lines):
-            words = line.split()
-            if not (words[n] in BLOCKED_WORDS or re.search(r"\([^)]*\)", words[n])):
-                lines[i] = BLOCK_LABEL + " " + line
+        for i, l in enumerate(lines):
+            if not blocked(l, n):
+                lines[i] = BLOCK_LABEL + " " + l
         n += 1
     print("iterated " + str(n) + " times")
     return n, lines
 
 
-def label_lines(lines):
+def label(lines):
     """Add line numbers to each line."""
-    return [f"{i+1}, {line}" for i, line in enumerate(lines)]
+    return [f"{i+1},{l}" for i, l in enumerate(lines)]
 
 
-def restore_lines(lines):
+def restore(lines):
     """Remove labels"""
     result = []
-    for line in lines:
-        line = line.replace(BLOCK_LABEL + " ", "")
-        line = line.replace(PARENTHESIS_LABEL, " ")
-        line = line.replace(NEWLINE_LABEL, "\n")
-        result.append(line)
+    for l in lines:
+        l = l.replace(BLOCK_LABEL + " ", "")
+        l = l.replace(PARENTHESIS_LABEL, " ")
+        l = l.replace(NEWLINE_LABEL, "\n")
+        result.append(l)
     return result
 
 
 def write_to_file(filename, lines):
     """Write the given lines to a new csv file."""
     with open(filename, "w", newline="") as f_out:
-        for line in lines:
-            f_out.write(line + "\n")
+        for l in lines:
+            f_out.write(l + "\n")
     print(f"Lines written to {filename}")
 
 
@@ -118,12 +141,12 @@ def process_file(input_file, output_file):
         write_to_file("debug/cleaned.csv", lines)
 
         print("now grouping")
-        lines = group_lines(lines)
+        lines = group(lines)
         print("grouping complete")
         write_to_file("debug/grouped.csv", lines)
 
         print("now blocking")
-        n, lines = block_lines(lines)
+        n, lines = block(lines)
         print("blocking complete")
         write_to_file("debug/blocked.csv", lines)
 
@@ -133,12 +156,12 @@ def process_file(input_file, output_file):
         write_to_file("debug/sorted.csv", lines)
 
         print("now labeling")
-        lines = label_lines(lines)
+        lines = label(lines)
         print("labeling complete")
         write_to_file("debug/labeled.csv", lines)
 
         print("now restoring")
-        lines = restore_lines(lines)
+        lines = restore(lines)
         print("restoring complete")
         write_to_file(output_file, lines)
 
